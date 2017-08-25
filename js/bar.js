38cfbcee0e15b600d621d74ebdc4289c56ac7c52
ruansheng <825307513@qq.com>
2017-08-26 01:38:15 +0800

    unicode

--- js/bar.js
+++ js/bar.js
@@ -1,40 +1,6 @@
 ﻿var auto_flush = true;
 $(function() {
 
-	function timestampToDate(timestamp){
-		var d = new Date(timestamp * 1000);
-		var year = d.getFullYear();
-		var month = d.getMonth();
-		var day = d.getDate();
-		var hour = d.getHours();
-		var minutes = d.getMinutes();
-		var seconds = d.getSeconds();
-
-		month = month + 1;
-		if(month < 10) {
-			month = "0" + month;
-		}
-		if(day < 10) {
-			day = "0" + day;
-		}
-		if(hour < 10) {
-			hour = "0" + hour;
-		}
-		if(minutes < 10) {
-			minutes = "0" + minutes;
-		}
-		if(seconds < 10) {
-			seconds = "0" + seconds;
-		}
-		var date = year + "-" + month + "-" + day + " " + hour + ":" + minutes + ":" + seconds;
-		return date;
-	}
-	
-	function dateToTimestamp(date) {
-		var timestamp = Date.parse(new Date(date.replace(/-/g,  "/")));
-		return timestamp / 1000;
-	}
-	
 	function setNowTime() {
 		// 当前时间戳
 		var now_timestamp = parseInt(new Date().getTime()/1000);
@@ -142,12 +108,31 @@ $(function() {
 	    $('#color-select-alert').modal({
 	      relatedTarget: this
 	    });
-	  });
+	});
 
 	$('#color-picker').colpick({
 		flat:true,
 		//layout:'hex',
 		submit:0
+	});
+
+	// unicode转换
+	$('#unicode-prompt-toggle').click(function() {
+	    $('#unicode-alert').modal({
+	      relatedTarget: this
+	    });
+	});
+	
+	$('#transform_unicode').click(function() {
+		var zhongweng_text = $('#zhongwen-text').val();
+		var unicode_text = encodeUnicode(zhongweng_text);
+		$('#unicode-text').val(unicode_text);
+	});
+
+	$('#transform_zhongwen').click(function() {
+		var unicode_text = $('#unicode-text').val();
+		var zhongweng_text = decodeUnicode(unicode_text);
+		$('#zhongwen-text').val(zhongweng_text);
 	});
 
 	// 二维码
---
+++ js/time_to_date.js
@@ -0,0 +1,33 @@
+﻿function timestampToDate(timestamp){
+	var d = new Date(timestamp * 1000);
+	var year = d.getFullYear();
+	var month = d.getMonth();
+	var day = d.getDate();
+	var hour = d.getHours();
+	var minutes = d.getMinutes();
+	var seconds = d.getSeconds();
+
+	month = month + 1;
+	if(month < 10) {
+		month = "0" + month;
+	}
+	if(day < 10) {
+		day = "0" + day;
+	}
+	if(hour < 10) {
+		hour = "0" + hour;
+	}
+	if(minutes < 10) {
+		minutes = "0" + minutes;
+	}
+	if(seconds < 10) {
+		seconds = "0" + seconds;
+	}
+	var date = year + "-" + month + "-" + day + " " + hour + ":" + minutes + ":" + seconds;
+	return date;
+}
+
+function dateToTimestamp(date) {
+	var timestamp = Date.parse(new Date(date.replace(/-/g,  "/")));
+	return timestamp / 1000;
+}
